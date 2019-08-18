17c8857e3857519d764e1e348f8da8962c4a0fca
johnjasa <johnjasa@umich.edu>
2019-08-18 17:15:15 -0400

    Working through standalone beam example

--- standalone_beam.py
+++ standalone_beam.py
@@ -3,46 +3,9 @@ from __future__ import print_function, division, absolute_import
 import numpy as np
 from scipy.sparse import coo_matrix
 from scipy.sparse.linalg import splu
+from scipy.optimize import minimize
 
 
-# Setup
-E = 1.
-L = 1.
-b = 0.1
-volume = 0.01
-
-num_elements = 50
-
-num_nodes = num_elements + 1
-
-force_vector = np.zeros(2 * num_nodes)
-force_vector[-2] = -1.
-
-
-h = np.ones((num_elements))
-
-
-# I_comp = MomentOfInertiaComp(num_elements=num_elements, b=b)
-I = 1./12. * b * h ** 3
-
-
-# comp = LocalStiffnessMatrixComp(num_elements=num_elements, E=E, L=L)
-L0 = L / num_elements
-coeffs = np.empty((4, 4))
-coeffs[0, :] = [12, 6 * L0, -12, 6 * L0]
-coeffs[1, :] = [6 * L0, 4 * L0 ** 2, -6 * L0, 2 * L0 ** 2]
-coeffs[2, :] = [-12, -6 * L0, 12, -6 * L0]
-coeffs[3, :] = [6 * L0, 2 * L0 ** 2, -6 * L0, 4 * L0 ** 2]
-coeffs *= E / L0 ** 3
-
-mtx = np.zeros((num_elements, 4, 4, num_elements))
-for ind in range(num_elements):
-    mtx[ind, :, :, ind] = coeffs
-
-K_local = np.zeros((num_elements, 4, 4))
-for ind in range(num_elements):
-    K_local[ind, :, :] = mtx[ind, :, :, ind] * I[ind]
-    
 def assemble_CSC_K(K_local):
     """
     Assemble the stiffness matrix in sparse CSC format.
@@ -100,23 +63,71 @@ def assemble_CSC_K(K_local):
     return coo_matrix((data, (rows, cols)), shape=(n_K, n_K)).tocsc()
 
 
-# comp = StatesComp(num_elements=num_elements, force_vector=force_vector)
-force_vector = np.concatenate([force_vector, np.zeros(2)])
 
-K = assemble_CSC_K(K_local)
-lu = splu(K)
+def beam_model(h, E=1., L=1., b=0.1, num_elements=50):
+    num_nodes = num_elements + 1
 
-d = lu.solve(force_vector)
+    force_vector = np.zeros(2 * num_nodes)
+    force_vector[-2] = -1.
 
-displacements = d[2 * num_nodes]
+    # I_comp = MomentOfInertiaComp(num_elements=num_elements, b=b)
+    I = 1./12. * b * h ** 3
 
-# comp = ComplianceComp(num_elements=num_elements, force_vector=force_vector)
-compliance = np.dot(force_vector, displacements)
 
-# comp = VolumeComp(num_elements=num_elements, b=b, L=L)
-L0 = L / num_elements
+    # comp = LocalStiffnessMatrixComp(num_elements=num_elements, E=E, L=L)
+    L0 = L / num_elements
+    coeffs = np.empty((4, 4))
+    coeffs[0, :] = [12, 6 * L0, -12, 6 * L0]
+    coeffs[1, :] = [6 * L0, 4 * L0 ** 2, -6 * L0, 2 * L0 ** 2]
+    coeffs[2, :] = [-12, -6 * L0, 12, -6 * L0]
+    coeffs[3, :] = [6 * L0, 2 * L0 ** 2, -6 * L0, 4 * L0 ** 2]
+    coeffs *= E / L0 ** 3
 
-volume = np.sum(h * b * L0)
+    mtx = np.zeros((num_elements, 4, 4, num_elements))
+    for ind in range(num_elements):
+        mtx[ind, :, :, ind] = coeffs
 
-# self.add_design_var('inputs_comp.h', lower=1e-2, upper=10.)
-# self.add_objective('compliance_comp.compliance')
+    K_local = np.zeros((num_elements, 4, 4))
+    for ind in range(num_elements):
+        K_local[ind, :, :] = mtx[ind, :, :, ind] * I[ind]
+
+    # comp = StatesComp(num_elements=num_elements, force_vector=force_vector)
+    force_vector = np.concatenate([force_vector, np.zeros(2)])
+
+    K = assemble_CSC_K(K_local)
+    lu = splu(K)
+
+    d = lu.solve(force_vector)
+
+    displacements = d[2 * num_nodes]
+
+    # comp = ComplianceComp(num_elements=num_elements, force_vector=force_vector)
+    compliance = np.dot(force_vector, displacements)
+    
+    return compliance
+
+    # self.add_design_var('inputs_comp.h', lower=1e-2, upper=10.)
+    # self.add_objective('compliance_comp.compliance')
+    
+def volume_function(h, L, b, num_elements):
+    L0 = L / num_elements
+
+    volume = np.sum(h * b * L0)
+
+num_elements=50
+E=1.
+L=1.
+b=0.1
+volume=0.01
+num_elements=50
+h0 = np.ones((num_elements))
+
+constraint_dict = {
+    'type' : 'eq',
+    'fun' : volume_function,
+    'args' : [h, L, b, num_elements]
+}
+
+result = minimize(beam_model, h0, args=[E, L, b, num_elements], constraints=constraint_dict)
+
+print(result.x)
